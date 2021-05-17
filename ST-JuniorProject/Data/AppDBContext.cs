68fce7edb9f311dc97a78562ec76c4204acc8d79
ZeroTech <shelikov98@yandex.ru>
2021-05-17 20:18:46 +0800

    Initial commit

---
+++ ST-JuniorProject/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ST_JuniorProject.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+    }
+}
